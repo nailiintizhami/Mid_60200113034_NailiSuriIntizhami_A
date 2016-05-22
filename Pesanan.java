d8caf6920454dd430381321155b7ba657f5642c6
nailiintizhami <60200113034@uin-alauddin.ac.id>
2016-05-22 21:09:40 +0800

    tugas mid

---
+++ Pesanan.java
@@ -0,0 +1,14 @@
+public class Pesanan {
+
+  public Integer idpesan;
+
+  public String Nama;
+
+  public Integer jumlah pesan;
+
+    public pelanggan order;
+
+  public void AddPesan() {
+  }
+
+}
---
+++ pelanggan.java
@@ -0,0 +1,14 @@
+public class pelanggan {
+
+  public Integer idpelanggan;
+
+  public Integer jumlah pesan;
+
+  public Integer total;
+
+    public Pesanan order;
+
+  public void AddCustomer() {
+  }
+
+}
